06900100766fdaf8d666c05baf7ab4037e77599d
HP <HP@DESKTOP-B0OK1FL>
2021-01-26 13:58:44 +0700

    Add project files.

---
+++ assingment 7 auction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace assingment_7_auction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item[] items = new Item[5];
+
+            items[0] = new Item("Phone", 5000);
+            items[1] = new Item("rc car", 1000);
+            items[2] = new Item("lego", 500);
+            items[3] = new Item("headset", 500);
+            items[4] = new Item("laptop", 10000 );
+
+            for (int i = 1; i < items.Length; i++)
+            {
+                if (items[i] != null)
+                {
+                    Console.WriteLine("Description: " + items[i].Getdescription() + "  Reserve Price: " + items[i].Getreserveprice());
+                }
+            }
+        }
+    }
+}
---
+++ assingment 7 auction/item.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assingment_7_auction
+{
+    class Item
+    {
+        private string desc;
+        private double resprice;
+        private int IDnum;
+        private double highestbid;
+
+        public Item(string desc, double resprice)
+        {
+            this.desc = desc;
+            this.resprice = resprice;
+        }
+
+        public string Getdescription()
+        {
+            return desc;
+        }
+
+        public double Getreserveprice()
+        {
+            return resprice;
+        }
+
+        public double Gethighestbid()
+        {
+            return highestbid;
+        }
+
+        public int GetbidderID()
+        {
+            return IDnum;
+        }
+
+        public bool Sethighestbid(double newbid)
+        {
+            if (newbid > highestbid && newbid > resprice)
+            {
+                highestbid = newbid;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("That is lower than the highest bid");
+                return false;
+            }
+        }
+
+        public void SetbidderID(int newID)
+        {
+            IDnum = newID;
+        }
+    }
+}
